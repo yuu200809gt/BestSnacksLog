1b0460c454d636a664177e6317dbf1d6837e6f6b
kunsei <yusei.2989gt@gmail.com>
2025-12-06 12:22:44 +0900

    add/TOPページ実装

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,6 +2,6 @@ class Users::SessionsController < Devise::SessionsController
   def guest_sign_in
     user = User.guest_sign_in
     sign_in user
-    redirect_to root_path, notice: "ゲストユーザーとしてログインしました。"
+    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
   end
 end
