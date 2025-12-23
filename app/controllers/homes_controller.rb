adc07bd508643bd9ffd0fad7dbe995e9e4d01bdd
kunsei <yusei.2989gt@gmail.com>
2025-12-24 02:17:11 +0900

    fix/最新投稿数を2から8に変更

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,5 @@
 class HomesController < ApplicationController
   def index
-  @snacks = Snack.all.order(created_at: :desc).limit(2)
+  @snacks = Snack.all.order(created_at: :desc).limit(8)
   end
 end
